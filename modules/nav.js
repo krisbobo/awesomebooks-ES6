f913b23a8e36ea4fc20060f84e273d1064482bc3
krisbobo <kiyongbohbo@gmail.com>
2022-07-28 03:09:46 +0100

    Add module for navigation

---
+++ modules/nav.js
@@ -0,0 +1,35 @@
+export default class Navigation {
+  constructor() {
+    this.year = document.querySelector('.year');
+    this.container = document.querySelector('.layout');
+    this.bookList = document.querySelector('.book');
+    this.contact = document.querySelector('.contact');
+    this.first = document.querySelector('.list');
+    this.second = document.querySelector('.add');
+    this.containerthird = document.querySelector('.contact-me');
+    this.title = document.querySelector('.book-header');
+  }
+
+  view() {
+    this.first.addEvenListener('click', (e) => {
+      e.preventDefault();
+      this.container.style.display = 'block';
+      this.bookList.style.display = 'none';
+      this.contact.style.display = 'none';
+    });
+    this.second.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.container.style.display = 'none';
+      this.bookList.style.display = 'flex';
+      this.contact.style.display = 'none';
+      this.title.style.display = 'none';
+    });
+    this.third.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.container.style.display = 'none';
+      this.bookList.style.display = 'none';
+      this.contact.style.display = 'block';
+      this.title.style.display = 'none';
+    });
+  }
+}
